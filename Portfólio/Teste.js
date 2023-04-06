71b2e7d1cab724042d68e3674e30b9e5e7de46bb
hellsing333 <cassiusishmael14@gmail.com>
2023-04-06 17:57:27 -0300

    novo exercício

---
+++ Portfólio/Teste.js
@@ -0,0 +1,10 @@
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.style.left= `${clientX}px`;
+  blob.style.top= `${clientY}px`;
+}
